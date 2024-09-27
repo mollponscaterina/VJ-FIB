20b129677e2aeaf9a23859804e43bba1add9d3aa
catee_moll <150155201+mollponscaterina@users.noreply.github.com>
2024-09-27 09:59:11 +0200

    Carpeta Binari

---
+++ images/enemics_gommba.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="enemics_gommba" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="enemics_gommba.png" width="64" height="64"/>
+</tileset>
